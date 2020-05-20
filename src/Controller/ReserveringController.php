7e9a09847981fe5f35b173c7c2bb5792dba02dcf
unknown <joey.oosenbrug@gmail.com>
2020-05-20 13:10:19 +0200

    swift

--- src/Controller/ReserveringController.php
+++ src/Controller/ReserveringController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
 /**
  * @Route("/reservering")
@@ -77,6 +79,7 @@ class ReserveringController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
 
     /**
      * @Route("/{id}", name="reservering_delete", methods={"DELETE"})
